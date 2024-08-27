de8050be7996032d7689a91887f7f01921cdea66
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-08-27 11:00:32 +0530

    Conditionals in thymeleaf
    Conditionals in thymeleaf using th:if ,| th:unless attribute | th:case |th:switch

---
+++ thymleafconditional/src/main/java/com/pooja/thymleaf/ThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.pooja.thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafApplication.class, args);
+	}
+
+}
---
+++ thymleafconditional/src/main/java/com/pooja/thymleaf/controller/FormController.java
@@ -0,0 +1,30 @@
+package com.pooja.thymleaf.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.pooja.thymleaf.model.UserForm;
+
+@Controller
+public class FormController {
+
+	
+	// handler method to handle user registration page
+	@GetMapping("/register")
+	public String userRegistrationPage(Model model) {
+		
+		UserForm userForm = new UserForm();
+		model.addAttribute("userForm", userForm);
+		
+		List<String> listProfession = Arrays.asList("Developer","Engineer", "HR");
+		model.addAttribute("listProfession",listProfession);
+		
+		return "register-form";
+
+		
+	}
+}
